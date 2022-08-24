da9b0c1fe086daba8a4a36a78c4f7f6b02507cdc
Jimmy Ngu <jimmynguyc@gmail.com>
2022-08-24 11:45:13 +0200

    Update version

--- player.rb
+++ player.rb
@@ -2,7 +2,7 @@ require_relative 'bet_request_service'
 
 class Player
 
-  VERSION = "Default Ruby folding player"
+  VERSION = "Negreanu Bot"
 
   def bet_request(game_state)
     BetRequestService.new(game_state).call
