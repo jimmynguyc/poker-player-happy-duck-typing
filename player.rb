b1aebc86888ff9f274c7723f3c69a3256a6f0b6d
Patrick Heisiph <patrick@heisiph.de>
2022-08-24 11:34:37 +0200

    game state parser

--- player.rb
+++ player.rb
@@ -4,6 +4,9 @@ class Player
   VERSION = "Default Ruby folding player"
 
   def bet_request(game_state)
+    current_buy_in = game_state["current_buy_in"]
+    pot = game_state["pot"]
+
     100
   end
 
