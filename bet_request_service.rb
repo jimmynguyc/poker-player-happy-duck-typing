4f5d10e9181bab6c5af100932acb23286cd46b5f
Jimmy Ngu <jimmynguyc@gmail.com>
2022-08-24 16:57:32 +0200

    always check or fold

--- bet_request_service.rb
+++ bet_request_service.rb
@@ -35,8 +35,7 @@ class BetRequestService
     return raise_by(100) if bet_big?
     return check_or_fold if hole_cards_are_shitty?
 
-      call_bet # stay in game
-
+      check_or_fold
   end
 
   private
